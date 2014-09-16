9011ed6c4f234fda3f44ed108c7bb92c4b7daf90
jordila <jla@riseup.net>
2014-09-16 11:33:56 -0400

    updated thumbs src path | Apache Alias /images hack III

--- collections-index.php
+++ collections-index.php
@@ -82,177 +82,160 @@
 	<div id="wrapper">
 	  <div id="gallery" class="clearfix">
 	    <figure>
-            <!-- Apache Alias : /collections-index-thumbs/images = /srv/datadisk01/admin/media/collections-index-thumbs/images/ + /videos -->
-              <a href="collection-AAT.php"> <img src="http://librebits.info/collections-index-thumbs/images/AAT-017_Patagonia_Atradecer en el mar_Peninsula Valdes-th.jpg" ></a>
+            <!-- Apache Alias : /images/collections-index-thumbs = /srv/datadisk01/admin/media/images/collections-index-thumbs/ + /videos -->
+              <a href="collection-AAT.php"> <img src="http://librebits.info/images/collections-index-thumbs/AAT-017_Patagonia_Atradecer en el mar_Peninsula Valdes-th.jpg" ></a>
               <figcaption>
 		Amaneceres y Atardeceres
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-AMR.php"> <img src="http://librebits.info/collections-index-thumbs/images/AMR-002_Patagonia_Gaviotas Cocineras_Chubut-th.jpg"></a>
+              <a href="collection-AMR.php"> <img src="http://librebits.info/images/collections-index-thumbs/AMR-002_Patagonia_Gaviotas Cocineras_Chubut-th.jpg"></a>
               <figcaption class="light">
 		Aves Marinas
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-AYO.php"><img src="http://librebits.info/collections-index-thumbs/images/AYO-006_Patagonia_Cosecha de Frambuesas_Granja Humus_El Bolson-th.jpg"></a>
+              <a href="collection-AYO.php"><img src="http://librebits.info/images/collections-index-thumbs/AYO-006_Patagonia_Cosecha de Frambuesas_Granja Humus_El Bolson-th.jpg"></a>
               <figcaption>
 		Artesanos y Oficios
               </figcaption>
 	    </figure>
+
+<!-- 2ª file [3x2] --> 
 	    
 	    <figure>
-              <a href="collection-AMR.php"><img src="http://librebits.info/collections-index-thumbs/images/AMR-002_Patagonia_Gaviotas Cocineras_Chubut-th.jpg"></a>
-              <figcaption>
-		Aves Marinas
-              </figcaption>
-	    </figure>
-	    
-	    <figure>
-              <a href="collection-AYO.php"><img src="http://librebits.info/collections-index-thumbs/images/AYO-006_Patagonia_Cosecha de Frambuesas_Granja Humus_El Bols-th.jpg"></a>
-              <figcaption>
-		Artesanos y Oficios
-              </figcaption>
-	    </figure>
-	    
-	    <figure>
-              <a href="collection-BAM.php"><img src="http://librebits.info/collections-index-thumbs/images/BAM-001_Patagonia_Salto Ballena Franca Austral_Peninsula Valdes-th.jpg"></a>
+              <a href="collection-BAM.php"><img src="http://librebits.info/images/collections-index-thumbs/BAM-001_Patagonia_Salto Ballena Franca Austral_Peninsula Valdes-th.jpg"></a>
               <figcaption>
 		Ballenas y Mamíferos
               </figcaption>
 	    </figure>
 
-<!-- 2ª 3x2 --> 
-
              <figure>
-             <a href="collection-BAN.php"> <img src="http://librebits.info/collections-index-thumbs/images/BAN-022_Patagonia_No-thofagus pumilio_Area Chalten_Santa Cruz-th.jpg"></a>
+             <a href="collection-BAN.php"> <img src="http://librebits.info/images/collections-index-thumbs/BAN-022_Patagonia_No-thofagus pumilio_Area Chalten_Santa Cruz-th.jpg"></a>
               <figcaption>
 		Bosques Andinos
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-CAM.php"> <img src="http://librebits.info/collections-index-thumbs/images/CAM-017_Patagonia_hacia el lago Puelo_Cubut-th.jpg"></a>
+              <a href="collection-CAM.php"> <img src="http://librebits.info/images/collections-index-thumbs/CAM-017_Patagonia_hacia el lago Puelo_Cubut-th.jpg"></a>
               <figcaption class="light">
 		Caminos
               </figcaption>
 	    </figure>
+
+<!-- 3ª file [3x2] --> 
 	    
 	    <figure>
-              <a href="collection-CIC.php"><img src="http://librebits.info/collections-index-thumbs/images/CIC-007_Patagonia_los deshielos en la primavera_area refugio Frey-th.jpg"></a>
+              <a href="collection-CIC.php"><img src="http://librebits.info/images/collections-index-thumbs/CIC-007_Patagonia_los deshielos en la primavera_area refugio Frey-th.jpg"></a>
               <figcaption>
 		Ciclo del Agua
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-CIE.php"><img src="http://librebits.info/collections-index-thumbs/images/CIE-021_Patagonia_cerca del glaciar Moreno_Santa Cruz-th.jpg"></a>
+              <a href="collection-CIE.php"><img src="http://librebits.info/images/collections-index-thumbs/CIE-021_Patagonia_cerca del glaciar Moreno_Santa Cruz-th.jpg"></a>
               <figcaption>
 		Cielos y Luces
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-COM.php"><img src="http://librebits.info/collections-index-thumbs/images/COM-017_Patagonia_Mar abierto_Peninsula Valdes-th.jpg"></a>
+              <a href="collection-COM.php"><img src="http://librebits.info/images/collections-index-thumbs/COM-017_Patagonia_Mar abierto_Peninsula Valdes-th.jpg"></a>
               <figcaption>
 		Costa Marina
               </figcaption>
 	    </figure>
- <!--   STOPped -->
+
+<!-- 4ª file [3x2] --> 
+
 	    <figure>
-              <a href="collection-ESH.php"><img src="http://librebits.info/collections-index-thumbs/images/ESH-012_Patagonia_Paisaje estepario en lago Argentino_Santa Cruz-th.jpg"></a>
+              <a href="collection-ESH.php"><img src="http://librebits.info/images/collections-index-thumbs/ESH-012_Patagonia_Paisaje estepario en lago Argentino_Santa Cruz-th.jpg"></a>
               <figcaption>
 		Estepas y Humedales
               </figcaption>
 	    </figure>
 
-<!-- 3ª  3x2  \!/ -->
-
-<figure>
-              <a href="collection-FAU.php"> <img src="http://librebits.info/collections-index-thumbs/images/FAU-004.Carpintero Magallanico - Magellanic Woodpecker-th.jpg" ></a>
+	    <figure>
+              <a href="collection-FAU.php"> <img src="http://librebits.info/images/collections-index-thumbs/FAU-004.Carpintero Magallanico - Magellanic Woodpecker-th.jpg" ></a>
               <figcaption>
 		Fauna Cordillerana
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-FLO.php"> <img src="http://librebits.info/collections-index-thumbs/images/FLO-016_Patagonia_Amancay. Alstroemeria aurea_Valle del Challhuaco-th.jpg" alt="Patagonia_Gaviotas-de-Olrog_Chubut"></a>
+              <a href="collection-FLO.php"> <img src="http://librebits.info/images/collections-index-thumbs/FLO-016_Patagonia_Amancay. Alstroemeria aurea_Valle del Challhuaco-th.jpg" alt="Patagonia_Gaviotas-de-Olrog_Chubut"></a>
               <figcaption class="light">
 		Flores y Flora Nativa
               </figcaption>
 	    </figure>
+
+<!-- 5ª file [3x2] --> 
 	    
 	    <figure>
-              <a href="collection-AYO.php"><img src="http://librebits.info/collections-index-thumbs/images/AYO-004_Patagonia_-Cosechando-frambuesas_Cabaña-Mico_El-Bolson.jpg" alt="Patagonia_-Cosechando-frambuesas_Cabaña-Mico_El-Bolson"></a>
-              <figcaption>
-		Artesanos y Oficios
-              </figcaption>
-	    </figure>
-	    
-	    <figure>
-              <a href="collection-GLA.php"><img src="http://librebits.info/collections-index-thumbs/images/GLA-023_Patagonia_pequeño iceberg del glaciar Perito Moreno varado en la costa_Santa Cruz-th.jpg"></a>
+              <a href="collection-GLA.php"><img src="http://librebits.info/images/collections-index-thumbs/GLA-023_Patagonia_pequeño iceberg del glaciar Perito Moreno varado en la costa_Santa Cruz-th.jpg"></a>
               <figcaption>
 		Glaciares
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-GYP.php"><img src="http://librebits.info/collections-index-thumbs/images/GYP-001_Patagonia_Valle del Rio Azul_El Bolson_Rio Negro-th.jpg" alt="Bosque-de-Coihues-en-la-niebla_Selva-Valdiviana"></a>
+              <a href="collection-GYP.php"><img src="http://librebits.info/images/collections-index-thumbs/GYP-001_Patagonia_Valle del Rio Azul_El Bolson_Rio Negro-th.jpg" alt="Bosque-de-Coihues-en-la-niebla_Selva-Valdiviana"></a>
               <figcaption>
 		Gente y Paisaje
               </figcaption>
 	    </figure>
-	    
+
 	    <figure>
-              <a href="collection-HIS.php"><img src="http://librebits.info/collections-index-thumbs/images/HIS-024_Patagonia_epoca fundacional_La Trochita u old patagonian express_circa 1972_Rio Negro-th.jpg"></a>
+              <a href="collection-HIS.php"><img src="http://librebits.info/images/collections-index-thumbs/HIS-024_Patagonia_epoca fundacional_La Trochita u old patagonian express_circa 1972_Rio Negro-th.jpg"></a>
               <figcaption>
 		Historia
               </figcaption>
 	    </figure>
 
-<!-- 4ª -->
-
+<!-- 6ª file [3x2] --> 
 
             <figure>
-              <a href="collection-LAG.php"> <img src="http://librebits.info/collections-index-thumbs/images/LAG-001_Patagonia_Nahuel Huapi en Angostura_Neuquen-th.jpg"></a>
+              <a href="collection-LAG.php"> <img src="http://librebits.info/images/collections-index-thumbs/LAG-001_Patagonia_Nahuel Huapi en Angostura_Neuquen-th.jpg"></a>
               <figcaption>
 		Los Lagos
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-MOV.php"> <img src="http://librebits.info/collections-index-thumbs/images/MOV-018_Patagonia_el Monte Tronador desde el Otto Meiling_Rio Negro-th.jpg"></a>
+              <a href="collection-MOV.php"> <img src="http://librebits.info/images/collections-index-thumbs/MOV-018_Patagonia_el Monte Tronador desde el Otto Meiling_Rio Negro-th.jpg"></a>
               <figcaption class="light">
 		Montañas y Valles
               </figcaption>
 	    </figure>
-	    
+
 	    <figure>
-              <a href="collection-OTO.php"><img src="http://librebits.info/collections-index-thumbs/images/OTO-002__Patagonia_Lenga en otoño_bosque andino patagonico_Rio Negro-th.jpg"></a>
+              <a href="collection-OTO.php"><img src="http://librebits.info/images/collections-index-thumbs/OTO-002__Patagonia_Lenga en otoño_bosque andino patagonico_Rio Negro-th.jpg"></a>
               <figcaption>
 		El Otoño
               </figcaption>
 	    </figure>
 
-      
+<!-- 7ª file [3x2] --> 
+
             <figure>
-              <a href="collection-PIN.php"> <img src="http://librebits.info/collections-index-thumbs/images/OTO-002__Patagonia_Lenga en otoño_bosque andino patagonico_Rio Negro-th.jpg"></a>
+              <a href="collection-PIN.php"> <img src="http://librebits.info/images/collections-index-thumbs/OTO-002__Patagonia_Lenga en otoño_bosque andino patagonico_Rio Negro-th.jpg"></a>
               <figcaption class="light">
 		Pingüino de Magallanes
               </figcaption>
 	    </figure>
 	    
 	    <figure>
-              <a href="collection-TDO.php"><img src="http://librebits.info/collections-index-thumbs/images/TDO-031_Patagonia_ski de travesia_Bariloche-th.jpg"></a>
+              <a href="collection-TDO.php"><img src="http://librebits.info/images/collections-index-thumbs/TDO-031_Patagonia_ski de travesia_Bariloche-th.jpg"></a>
               <figcaption>
 		Trekking y Deportes Outdoor
               </figcaption>
 	    </figure>
 
             <figure>
-              <a href="collection-TEX.php"><img src="http://librebits.info/collections-index-thumbs/images/TEX-018_Patagonia_ultima nive de primavera-th.jpg"></a>
+              <a href="collection-TEX.php"><img src="http://librebits.info/images/collections-index-thumbs/TEX-018_Patagonia_ultima nive de primavera-th.jpg"></a>
               <figcaption>
 		Texturas y Abstractas
               </figcaption>
