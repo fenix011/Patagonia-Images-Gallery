07aa6f9fb6893432b851ed52e48706085465de9f
jordila <jla@riseup.net>
2014-09-25 00:26:43 -0400

    reordenando styles.css en index.php : fixed navbar text color

--- index.php
+++ index.php
@@ -42,13 +42,14 @@
 
 <!-- ++ as per Bootstrap-Images-Gallery -->
 
-<!--    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"> -->
+
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <link rel="stylesheet" href="css/demo.css">
     
 
