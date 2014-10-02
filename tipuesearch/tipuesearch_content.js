3abf44c5a498aed9a6f46a32e807a3bb32415433
jordila <jla@riseup.net>
2014-10-02 19:07:59 -0400

    +- path tipuesearch_Content.js  dev.patag...

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,7 +1,7 @@
 var tipuesearch = {"pages": [
-     {"title": "Patagonia Images |   Quién Soy   ", "text": "ramitas chile paine",  "tags": "texturas", "loc": "http://librebits.info/about.php"},
+     {"title": "Patagonia Images |   Quién Soy   ", "text": "ramitas chile paine",  "tags": "texturas", "loc": "http://dev.patagonia-images.com/about.php"},
 
-     {"title": "Patagonia Images |   Servicios   ", "text": "Servicios de fotografía y video simultáneos ", "tags": "texturas", "loc": "http://librebits.info/services.php"},
+     {"title": "Patagonia Images |   Servicios   ", "text": "Servicios de fotografía y video simultáneos ", "tags": "texturas", "loc": "http://dev.patagonia-images.com/services.php"},
 
      {"title": "Tipue Search Live mode demo", "text": "This is a demo of Tipue Search Live mode.", "tags": "", "loc": "http://www.tipue.com/search/demos/live"},
      {"title": "Tipue Search docs", "text": "If you haven't already done so, download Tipue Search. Copy the tipuesearch folder to your site.", "tags": "documentation", "loc": "http://www.tipue.com/search/docs"}
