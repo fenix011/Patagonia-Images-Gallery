89fed613178d544657eea3ddc585506d5d63387f
root <root@patagon01>
2014-10-06 18:15:00 +0200

    ++ Bs navbar-search |tipuesearch box

--- components/php/navbar.php
+++ components/php/navbar.php
@@ -17,6 +17,30 @@
 	      <li><a href="about.php">Quién Soy</a></li>
               <li><a href="mision.php">Misión</a></li>
               <li><a href="contact.php">Contacto</a></li>
+
+<!-- as per getboostrap../components/#navbar-forms  --->
+
+<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <span class="glyphicon glyphicon-search"></span>
+    <input type="text" class="form-control" placeholder="Ejemplo: ballenas,flores...">
+  </div>
+  <button type="submit" class="btn btn-default">Buscar</button>
+</form>
+
+<!-- / getbs -->
+
+              <li>
+
+<!-- tipue serch box form -->
+                    <form action="search.php">
+                      <input type="text" name="q" id="tipue_search_input" autocomplete="off" required>
+                    </form>
+
+              </li>
+
+
+
             </ul>
         </div>
     </div>
