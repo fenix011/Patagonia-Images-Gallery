70daed51e4e3a2a87ac888bb832930445a8e7916
jordila <jordilopezamat@gmail.com>
2014-10-07 16:27:32 +0200

    +- tipuesearch style site (standar) font Helvetica | ++ Listad menu en navbar

--- search.php
+++ search.php
@@ -87,13 +87,13 @@
 
 	      <div class="row">
 		<div class="col-md-1"></div>
-		<div class="col-md-10">
+		<div class="col-md-9">
 		  <div id="content">
 
    <!-- main content: tipue search results:  -->
 		    <div id="tipue_search_content"></div>
    <!-- /tipue ...-->
-                  <div class="col-md-1"></div>
+                  <div class="col-md-2"></div>
 	          </div>
 		</div>
 	      </div>
